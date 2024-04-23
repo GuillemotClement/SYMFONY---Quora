6e343506f3ce63bed2dd53937d09be67adf2f47f
Clement Glt <guillemot.clement@protonmail.com>
2024-04-23 17:51:02 +0200

    Ajout du formilaire creation question

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\QuestionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // On vient récupérer la classe qui gère le formulaire
+        $formQuestion = $this->createForm(QuestionType::class);
+
+        //on récupère les données de la requête
+        $formQuestion->handleRequest($request);
+
+        //si le formulaire est soumis et valid
+        if($formQuestion->isSubmitted() && $formQuestion->isValid()){
+            //on affiche les données de la requête
+            dump($formQuestion->getData());
+        }
+
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'form' => $formQuestion->createView(),
         ]);
     }
 }
