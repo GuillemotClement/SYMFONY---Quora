7a2d238d46e9e8ac0e2a7d53534bcaf56a0c207c
Clement Glt <guillemot.clement@protonmail.com>
2024-04-29 15:41:07 +0200

    ajout de la connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -35,10 +36,18 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenUtils): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        if($this->getUser()){
+            return $this->redirectToRoute('home');
+        }
+
+        $error = $authenUtils->getLastAuthenticationError();
+        $username = $authenUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error' => $error,
+            'username'=>$username
         ]);
     }
 
