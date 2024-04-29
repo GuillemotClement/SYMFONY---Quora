60e8562c18e257a4243cd80a0fb446d0068691ab
Clement Glt <guillemot.clement@protonmail.com>
2024-04-29 16:21:28 +0200

    protection des routes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,18 +6,20 @@ use App\Entity\Comment;
 use App\Entity\Question;
 use App\Form\CommentType;
 use App\Form\QuestionType;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
+    
     public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
         //on commence par créer la question
         $question = new Question();
         // On vient récupérer la classe qui gère le formulaire et on le lie à l'entity
@@ -29,6 +31,7 @@ class QuestionController extends AbstractController
             //on passe les valeur par défaut
             $question->setNumberOfResponse(0); //par défaut il y a 0 réponse
             $question->setRating(0); //par défaut la note est à 0
+            $question->setAuthor($user);
             $question->setCreatedAt(new \DateTimeImmutable()); //par défaut on utilise le dateTime
             //on vient utiliser entity manager pour ajouter les deux autres champs renseigner par l'user
             //on utilise persist() pour sauvegarder les données saisi
@@ -47,43 +50,48 @@ class QuestionController extends AbstractController
         ]);
     }
 
-    #[Route(
-        path: "/question/{id}", name:"question_show"
-    )]
+    #[Route(path: "/question/{id}", name:"question_show")]
     public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
-        //on vient créer le commentaire
-        $comment = new Comment();
-        // on vient lier la classe du formulaire, et la classe entity
-        $commentForm = $this->createForm(CommentType::class, $comment);
-        //on récupère la requête
-        $commentForm->handleRequest($request);
+        $options = [
+            'question' => $question
+        ];
+        
+        $user = $this->getUser();
 
-        if($commentForm->isSubmitted() && $commentForm->isValid()){
-            //on ajoute les valeurs par défaut
-            $comment->setCreatedAt(new \DateTimeImmutable());
-            $comment->setRating(0);
-            //on set directement la ref à la question
-            $comment->setQuestion($question);
-            //on vient mettre à jour le nombre de response à une question
-            $question->setNumberOfResponse($question->getNumberOfResponse() +1);
-            //on persiste la requete avec l'entity manager
-            $em->persist($comment);
-            $em->flush();
-            //on vient afficher un message flash
-            $this->addFlash('success', "Réponse ajoutée avec succès");
-            //on refresh la page avec uri
-            return $this->redirect($request->getUri());
+        if($user){
+                //on vient créer le commentaire
+            $comment = new Comment();
+            // on vient lier la classe du formulaire, et la classe entity
+            $commentForm = $this->createForm(CommentType::class, $comment);
+            //on récupère la requête
+            $commentForm->handleRequest($request);
+
+            if($commentForm->isSubmitted() && $commentForm->isValid()){
+                //on ajoute les valeurs par défaut
+                $comment->setCreatedAt(new \DateTimeImmutable());
+                $comment->setRating(0);
+                //on set directement la ref à la question
+                $comment->setQuestion($question);
+                $comment->setAuthor($user);
+                //on vient mettre à jour le nombre de response à une question
+                $question->setNumberOfResponse($question->getNumberOfResponse() +1);
+                //on persiste la requete avec l'entity manager
+                $em->persist($comment);
+                $em->flush();
+                //on vient afficher un message flash
+                $this->addFlash('success', "Réponse ajoutée avec succès");
+                //on refresh la page avec uri
+                return $this->redirect($request->getUri());
+            }
+            $ptions['form'] = $commentForm->createView();
         }
-
-        return $this->render('question/show.html.twig', [
-            'question' => $question,
-            //on vient ajouter le form dans le render et la méthode createView car formulaire
-            'form' => $commentForm->createView()
-        ]);
+       
+        return $this->render('question/show.html.twig', $options);
     }
 
     #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function ratingQuestion(Question $question, int $score, EntityManagerInterface $em, Request $request)
     {
         $question->setRating($question->getRating() + $score);
@@ -93,6 +101,7 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/comment/rating/{id}/{score}', name: 'comment_rating')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function ratingComment(Comment $comment, int $score, EntityManagerInterface $em, Request $request)
     {
         $comment->setRating($comment->getRating() + $score);
