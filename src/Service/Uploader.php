6b8d25a2a7cd822149ef89c130de86223e14bba4
Clement Glt <guillemot.clement@protonmail.com>
2024-04-30 22:33:25 +0200

    Refacto du CSS en scss

--- src/Service/Uploader.php
+++ src/Service/Uploader.php
@@ -8,21 +8,21 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class Uploader
 {
-	public function __construct(private Filesystem $fs, private $profilFolder, private $profilFolderPublic)
+	public function __construct(private Filesystem $fs, private $profileFolder, private $profileFolderPublic)
 	{
 		
 	}
 
 	public function uploadProfileImage(UploadedFile $picture, string $oldPicturePath = null): string
 	{
-		$folder = $this->profilFolder;
+		$folder = $this->profileFolder;
 		$ext = $picture->guessExtension() ?? 'bin';
 		$filename = bin2hex(random_bytes(10)) . '.' . $ext;
 		$picture->move($folder, $filename);
 		if($oldPicturePath){
 			$this->fs->remove($folder . '/' . pathinfo($oldPicturePath, PATHINFO_BASENAME));
 		}
-		return $this->profilFolderPublic . '/' . $filename;
+		return $this->profileFolderPublic . '/' . $filename;
 	}
 
 
