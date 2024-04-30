dea489b4f88bb5702bd73f1ffa65bd9d4d18b575
Clement Glt <guillemot.clement@protonmail.com>
2024-04-30 11:45:00 +0200

    Add mail pour inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,32 +5,49 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 
 class SecurityController extends AbstractController
 {
+
+    public function __construct(
+        private FormLoginAuthenticator $authenticator
+    ) {
+    }
+
+
+
     #[Route('/signup', name: 'signup')]
-    public function signup(UserAuthenticatorInterface $autoInter, AbstractLoginFormAuthenticator $loginForm, Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    public function signup(UserAuthenticatorInterface $userAuthenticator, Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, MailerInterface $mailer): Response
     {
         $user = new User();
         $userForm = $this->createForm(UserType::class, $user);
-        
         $userForm->handleRequest($request);
-
         if($userForm->isSubmitted() && $userForm->isValid()){
             $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
             $em->persist($user);
             $em->flush();
             $this->addFlash('success', 'Bienvenue sur Quora');
+            $email = new TemplatedEmail();
+            $email->to($user->getEmail());
+            $email->subject('Bienvenue sur Quora');
+            $email->htmlTemplate('@email_templates/welcome.html.twig');
+            $email->context([
+                'username'=> $user->getFirstname()
+            ]);
+            $mailer->send($email);
             // inscription et redirect vers acceuil 
-            return $autoInter->authenticateUser($user, $loginForm, $request);
+            return $userAuthenticator->authenticateUser($user, $this->authenticator, $request);
         }
 
         return $this->render('security/signup.html.twig', [
